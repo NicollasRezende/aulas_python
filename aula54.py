69b6df09afad97d3f79b71d1f1bd941b59917f71
Nicollas <nicollaspereira025@gmail.com>
2023-07-28 11:29:27 -0300

    aulas_python

--- aula54.py
+++ aula54.py
@@ -1,34 +1,51 @@
-'''
-faça uma lista de compras 
-onde o usuario pode inserir apagar e listar os valores
-nao permita que o programa quebr com erros de indices inexistentes na lista
-
-'''
-
 
 lista_de_compras = []
-lista_de_compras_enumerada = enumerate(lista_de_compras)
+
+import os
 
 while True:
-    usuario = input('Deseja [I]inserir, [C]lear ou [L]istar: ')
+    #recebe os comandos
     
+    print('selecione uma opçao')
+    usuario = input('Deseja [i]inserir, [r]emover ou [l]istar: ')
+    os.system('clear')
+    #inserir valores
     if usuario == 'i':
-        lista_de_compras.append(input('O que deseja inserir: '))
-        print(lista_de_compras)
+        
+        valor = input('O que deseja inserir: ')
+        lista_de_compras.append(valor)
+        os.system('clear')
         continue
-
-    elif usuario == 'c':
-        lista_de_compras.clear
-        print(lista_de_compras)
-        continue
-
+    #listar valores
     elif usuario == 'l':
-        for elemento in lista_de_compras_enumerada:
-            print(elemento)
-        continue
-
+        
+        for i, valor in enumerate(lista_de_compras):
+            print(i, valor)
+            continue
+        if len(lista_de_compras) == 0:
+            print('Nada para listar')
+        os.system('clear')
+    #remove os valores
+    elif usuario == 'r':
+        
+        for i, valor in enumerate(lista_de_compras):
+            print(i, valor)
+        indice_str = input('O que deseja apagar: ')
+        try:
+            indice = int(indice_str)
+            del lista_de_compras[indice]
+            os.system('clear')
+            continue
+        except ValueError:
+            print('Digite apenas numeros')
+            os.system('clear')
+            continue
+        except IndexError:
+            print('Esse indice nao existe')
+            os.system('clear')
+            continue
+    #se nenhuma das opçoes escolhidas for digitada
     else:
-        print('voce nao digitou nada')
-        continue
+        print('por favor escolha uma das opçoes [i], [l], [r]')
 
 
