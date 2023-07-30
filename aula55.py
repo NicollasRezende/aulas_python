4ee459eefc2392c008629f9b5bfd8331cb5cf9c0
Nicollas <nicollaspereira025@gmail.com>
2023-07-30 11:36:51 -0300

    aulas_python

---
+++ aula55.py
@@ -0,0 +1,14 @@
+lista = [
+        ['helena', 'carlos', ], 
+        ['jorge', ],
+        ['lucas', 'souza', 'abelha', (0, 10, 20, 30, 40, 50, ) ]
+        ]
+
+
+
+
+print(lista[0][1])
+print(lista[1][0])
+print(lista[2][0])
+print(lista[2][1])
+print(lista[2][3][2])
