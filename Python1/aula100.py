22e0e2642e84e9f33506fa043a9e9d600cae350f
NICOLLAS PEREIRA REZENDE <nicollaspereira025@gmail.com>
2023-08-28 10:50:54 -0300

    aula_python

---
+++ Python1/aula100.py
@@ -0,0 +1,15 @@
+listaA = [2,4,6,8]
+listaB = [3,6,9,12]
+
+
+lista_soma = []
+
+
+for i in range(len(listaA)):
+    lista_soma.append(listaA[i] + listaB[i])
+
+
+
+print(lista_soma)
+
+
---
+++ Python1/aula99.py
@@ -0,0 +1,20 @@
+# Exercício - Unir listas
+# Crie uma função zipper (como o zipper de roupas)
+# O trabalho dessa função será unir duas
+# listas na ordem.
+# Use todos os valores da menor lista.
+# Ex.:
+# ['Salvador', 'Ubatuba', 'Belo Horizonte']
+# ['BA', 'SP', 'MG', 'RJ']
+# Resultado
+# [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+
+
+
+from itertools import zip_longest
+
+l1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+l2 = ['BA', 'SP', 'MG', 'RJ']
+print(list(zip(l1, l2)))
+print(list(zip_longest(l1, l2, fillvalue='SEM CIDADE')))
